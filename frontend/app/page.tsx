3072b150af083862bd616ee4336ea96da0a410a6
Shanuka Lakshan <89212510+ShanukaLakshan@users.noreply.github.com>
2024-12-09 10:11:51 +0530

    Refactor frontend structure: remove unused components, update Tailwind configuration, and add utility functions

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,14 @@
-"use client";
-import Head from "next/head";
-import MainContent from "./components/MainContent";
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Questionnaire</title>
-        <meta name="description" content="A simple questionnaire form" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <MainContent />
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+       
+        
       </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        
+      </footer>
     </div>
   );
 }
