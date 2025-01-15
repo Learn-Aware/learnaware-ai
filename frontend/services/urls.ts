67d42f5e2b605034cf0dc914afcb7c105083d7f2
ShanukaLakshan <lakshan.20@uom.lk>
2025-01-15 17:00:50 +0530

    Update BASE_URL to point to the new API endpoint for QubitsGain

--- frontend/services/urls.ts
+++ frontend/services/urls.ts
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://127.0.0.1:8000/";
-export const BASE_URL = "http://216.244.86.210:8000/";
+export const BASE_URL = "https://api.qubitsgain.com/";
 
 export const SOCRATIC_TUTOR = BASE_URL + "socratic_tutor/api/socratic_tutor";
 export const START_SESSION = BASE_URL + "socratic_tutor/start-session";
